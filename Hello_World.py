fdfb4d3d0c5a54eb0f43705ce4c6167ab4020930
BenSilk <34220023+BenSilk@users.noreply.github.com>
2017-12-05 11:37:27 +1300

    Began the project

---
+++ Hello_World.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  5 11:20:20 2017
+
+@author: Ben
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.rooute('/')
+def hello():
+    return("Hello World")
